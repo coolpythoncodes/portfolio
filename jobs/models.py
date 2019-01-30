06b021c4ad77893696e99d272704aa418477b318
coolpythoncodes <chijiokeibe15@gmail.com>
2019-01-31 09:56:57 +1200

    fixed bug

--- jobs/models.py
+++ jobs/models.py
@@ -9,9 +9,6 @@ class Jobs(models.Model):
     upload_date = models.DateTimeField(auto_now_add=True)
     
 
-    def __str__(self):
-        return self.upload_date 
-
     def get_absolute_url(self):     
         return reverse('home', args=[str(self.id)])
 
