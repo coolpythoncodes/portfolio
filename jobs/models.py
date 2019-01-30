5f4179f6e9211faf0db79c14d91c03d98f9a50d7
coolpythoncodes <chijiokeibe15@gmail.com>
2019-01-31 09:20:13 +1200

    configure Jobs models

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+
+class Jobs(models.Model):
+    image = models.ImageField(upload_to="images/")
+    upload_date = models.DateTimeField(auto_now_add=True)
+    
+
+    def __str__(self):
+        return self.upload_date 
+
+    def get_absolute_url(self):     
+        return reverse('home', args=[str(self.id)])
+
+   
+
