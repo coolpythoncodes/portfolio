47414b264dc13a254df29b219a7b478c1900e840
coolpythoncodes <chijiokeibe15@gmail.com>
2019-01-31 09:59:48 +1200

    added template folder to save html files

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['template',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
