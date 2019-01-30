2787cc93e3a6e59fb3b45daca4c8580ecaa0a0e6
coolpythoncodes <chijiokeibe15@gmail.com>
2019-01-31 11:17:49 +1200

    created a view  to delete a job entry in the database

--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,8 @@
 from django.views.generic import ListView
+from django.views.generic.edit import DeleteView
+from django.urls import reverse_lazy
 from .models import Jobs
+
 
 # Create your views here.
 
@@ -9,3 +12,11 @@ class JobsListView(ListView):
     context_object_name = 'jobs'
     template_name='home.html'
     queryset = Jobs.objects.order_by('-upload_date')
+
+
+
+class JobsDeleteView(DeleteView):
+    model = Jobs
+    template_name = 'delete_job.html'
+    success_url = reverse_lazy('home')
+
