8ed02d118789479160c0e2f4c9e11a8d45133c1e
coolpythoncodes <chijiokeibe15@gmail.com>
2019-02-01 00:17:22 +1200

    url route to change job image

--- jobs/urls.py
+++ jobs/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.JobsListView.as_view(), name='home'),
     path('remove/<int:pk>', views.JobsDeleteView.as_view(),  name='remove'),
-    path('new', views.JobsCreateView.as_view(), name='new')
+    path('new', views.JobsCreateView.as_view(), name='new'),
+    path('change/<int:pk>', views.JobsUpdateView.as_view(), name='change')
 ]
