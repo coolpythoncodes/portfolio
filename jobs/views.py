434f4803c85456d8453d4d4f85c8855c1f81db3a
coolpythoncodes <chijiokeibe15@gmail.com>
2019-01-31 23:30:45 +1200

    view to create new job image in database

--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView
-from django.views.generic.edit import DeleteView
+from django.views.generic.edit import CreateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Jobs
 
@@ -19,4 +19,12 @@ class JobsDeleteView(DeleteView):
     model = Jobs
     template_name = 'delete_job.html'
     success_url = reverse_lazy('home')
+
+
+
+class JobsCreateView(CreateView):
+    model = Jobs
+    fields = ['image']
+    template_name = "new_job.html"
+
 
