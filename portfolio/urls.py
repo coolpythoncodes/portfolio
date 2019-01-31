594e1f96535e9d3e8fd716c9b43c183b45c0cb4e
coolpythoncodes <chijiokeibe15@gmail.com>
2019-02-01 04:12:27 +1200

    added missing quotes

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -20,6 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include(django.contrib.auth.urls)),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('jobs.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
