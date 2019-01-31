4c82e8b899401fa7784d892f2622c13c5adece7c
coolpythoncodes <chijiokeibe15@gmail.com>
2019-01-31 23:32:30 +1200

    url route to add a new job image

--- jobs/urls.py
+++ jobs/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.JobsListView.as_view(), name='home'),
     path('remove/<int:pk>', views.JobsDeleteView.as_view(),  name='remove'),
+    path('new', views.JobsCreateView.as_view(), name='new')
 ]
