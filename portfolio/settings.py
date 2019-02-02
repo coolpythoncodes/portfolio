2592ef896f00fd9927c80e762a6902389db96f8d
coolpythoncodes <chijiokeibe15@gmail.com>
2019-02-03 11:08:28 +1200

    fixing deploying issue

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -85,12 +85,8 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('NAME'),
-        'USER': config('USER'),
-        'PASSWORD': config('PASSWORD'),
-        'HOST': config('HOST'),
-        'PORT': config('PORT', cast=int),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
