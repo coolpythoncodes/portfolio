0c29ac95ddcf341c56a0e01b770f0fdc22bab23d
coolpythoncodes <chijiokeibe15@gmail.com>
2019-02-03 01:40:10 +1200

    added missing comma

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/', include('accounts/urls'))
+    path('accounts/', include('accounts/urls')),
     path('', include('jobs.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
