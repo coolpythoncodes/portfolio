66b1f66fd37e79e374d17a298a1bc9fe0c2c3990
coolpythoncodes <chijiokeibe15@gmail.com>
2019-02-03 04:10:18 +1200

    Reset password with sendgrid email

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -137,3 +137,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 LOGIN_REDIRECT_URL ='home'
 LOGOUT_REDIRECT_URL ='home'
+
+# Reset password with sendgrid email
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
