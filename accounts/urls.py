ef690d010529ab409d9e7e0a05e238260f3df17d
coolpythoncodes <chijiokeibe15@gmail.com>
2019-02-03 01:59:04 +1200

    ModuleNotFoundError [fixed]

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/', views.SignUpView.as_view(), name='signup')
+    path('signup/', views.SignUpView.as_view(), name='signup'),
 ]
